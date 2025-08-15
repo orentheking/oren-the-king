767e43ff7bf12f74c8b5e10e3edf1532635701fc
orentheking <orena@appcard.com>
2025-08-15 16:12:03 +0300

    Initial commit - Oren the King project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '2rem' }}>
+      <h1>Oren the King 👑</h1>
+    </div>
+  );
+}
+
+export default App;
